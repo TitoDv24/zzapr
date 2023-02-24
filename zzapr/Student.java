1ea6c5ec9b821bbb6959781199cd30fd6798de75
Salvatore Corallo <salvo.corallo98@gmail.com>
2023-02-24 11:21:20 +0100

    init commit

--- zzapr/Student.java
+++ zzapr/Student.java
@@ -2,6 +2,6 @@ package zzapr;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Salvatore Corallo, University of Palermo, Electronics Engineering. ");
     }
 }
