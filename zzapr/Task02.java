20c53a250492ec4c2955a8e16b4da9e969d3c2de
st70365 <salvo.corallo98@gmail.com>
2023-03-10 11:57:53 +0100

    Task2

--- zzapr/Task02.java
+++ zzapr/Task02.java
@@ -12,15 +12,35 @@ public class Task02 {
         System.out.print("Write which will be the last value of the table: ");
         int end = input.nextInt();
 
+        if (start > end) {
+            System.out.print("You Choosed the Wrong Numbers.");
+        } else {
+            System.out.print("  ");
+            for (int i = start - 1; i <= end; i++) {
+                if (i > start) {
+                    System.out.printf("%d|", i);
+                } else if (i == start) {
+                    System.out.print("   ");
+                    for (int a = 0; a < (end - start + 1) * 6; a++) {
+                        System.out.print("-");
+                    }
+                    System.out.print("\n");
+                    System.out.printf("%d|", i);
+                }
+                for(int  j = start; j <= end; j++){
+                    if(i == start -1){
+                        System.out.printf("%6d", j);
+                    }
+                    else{
+                        System.out.printf("%6d", i*j);
+                    }
+                }
 
-        //rows
-        for (int i = start; i <= end; i++ ) {
-            //columns
-            for(int j =start; j<=end; j++ ) {
-                System.out.printf("%6d", i * j);
+                System.out.print("\n");
+
+                System.out.println();
             }
-            System.out.println();
         }
-
     }
 }
+
