1cd5ac379f465372c76c5a18c28c5aa8faf3379b
salvo <salvo.corallo98@gmail.com>
2023-03-21 16:35:36 +0100

    2° attempt

--- zzapr/lesson4/Main.java
+++ zzapr/lesson4/Main.java
@@ -1,12 +1,33 @@
-package zzapr.lesson4;
+package zzapr.zzapr.lesson4;
 
-import java.util.Scanner;
+import java.util.Objects;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Arrays matrixA = new Arrays(4,5);
+		Arrays matrixA = new Arrays(4, 1);
 		matrixA.generateRandomElements(-20, +20);
+		System.out.println("Matrix A:");
 		System.out.println(matrixA);
+
+		Arrays matrixB = new Arrays(1,4);		matrixB.generateRandomElements(-20, +20);
+
+		System.out.println("Matrix B:");
+		System.out.println(matrixB);
+
+		Arrays matrixC = matrixA.sumMatrix(matrixB);
+		System.out.print("matrix A + matrix B = \n");
+		if (!Objects.isNull(matrixC)) {
+			System.out.println(matrixC);
+		} else {
+			System.out.println("it is impossible to calculate\n" + "matrix A size is different from matrix B size.");
+		}
+		Arrays matrixD = matrixA.multiplyMatrices(matrixB);
+		System.out.print("matrix A * matrix B = \n");
+		if (!Objects.isNull(matrixD)) {
+			System.out.println(matrixD);
+		} else {
+			System.out.println("it is impossible to calculate the multiplication\n");
+		}
 	}
 }
